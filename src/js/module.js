ea77a6a155533c01ea05df56f3a50b25cb1c31e3
Jeremy Glebe <jeremyglebe@gmail.com>
2024-05-16 15:00:10 -0500

    I don't remember what changed in the packs tbh

--- src/js/module.js
+++ src/js/module.js
@@ -4,26 +4,6 @@ Hooks.once('init', () => {
   console.log(`${moduleId} | Hook: init`);
 });
 
-// Hooks.on('swadePreRollAttribute', (actor, attribute, roll, modifiers, options) => {
-//   console.log(`${moduleId} | Hook: swadePreRollAttribute`);
-//   console.log(`Actor: ${actor.name}`);
-//   console.log(`Attribute: ${attribute}`);
-//   console.log(`Roll: ${roll}`);
-//   console.log(`Modifiers: ${modifiers}`);
-//   console.log(`Options: ${options}`);
-//   // modifiers.push({ label: 'My Modifier', value: 2, ignore: true });
-// });
-
-// function logPreRollSkill(actor, skill, roll, modifiers, options) {
-//   console.log(`${moduleId} | Hook: swadePreRollSkill`);
-//   console.log('Actor:', actor);
-//   console.log('Skill:', skill);
-//   console.log('Roll:', roll);
-//   console.log('Modifiers:', modifiers);
-//   console.log('Options:', options);
-// }
-// Hooks.on('swadePreRollSkill', logPreRollSkill);
-
 // Import scripts that should be executed but have no exports
 import './modifiers/toggle-by-label.js';
 // Hindrances
