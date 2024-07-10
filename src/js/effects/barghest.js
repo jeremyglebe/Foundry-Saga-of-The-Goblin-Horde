3210b41427cbd06f8e01fcc9b0ea38763b133665
Jeremy Glebe <jeremyglebe@gmail.com>
2024-07-09 23:39:16 -0500

    fixed some barghest automation bugs

--- src/js/effects/barghest.js
+++ src/js/effects/barghest.js
@@ -2,13 +2,15 @@ import * as jsonData from '../../module.json' assert { type: 'json' };
 const moduleId = jsonData.id;
 
 // Function to run when updateActiveEffect is called
-async function barghestUpdateActiveEffect(effect, toggleData, _0, _1) {
+async function barghestUpdateActiveEffect(effect, toggleData, _, activatingUserId) {
   console.log(`${moduleId}:barghest.js | Hook: updateActiveEffect`);
+  console.log(arguments);
+  const thisUserId = game.userId;
   const { disabled } = toggleData;
-  console.log('disabled: ', disabled);
+
   // Checks if effect is being toggled (disabled will be true or false, if undefined then this update isn't a toggle)
-  if (disabled !== undefined) {
-    console.log('running toggle effect');
+  // Also checks that this user is the one toggling the effect (to avoid duplicate toggles from GM and owner of actor)
+  if (disabled !== undefined && thisUserId === activatingUserId) {
     // Determines if the effect is a barghest toggle effect, then runs the appropriate function
     if (effect.changes.find((change) => change.key === 'barghest-toggle-hybrid')) {
       await barghestToggleHybridForm(effect, disabled);
@@ -24,6 +26,7 @@ Hooks.on('updateActiveEffect', barghestUpdateActiveEffect);
 // Function to run when the Hybrid Form toggle is updated
 async function barghestToggleHybridForm(effect, disabled) {
   const { actor } = effect;
+  const { favorite } = effect.flags.swade;
 
   const itemId = 'Q5xS4QCulqraVtcv';
   const itemSwid = 'barghest-hybrid-form';
@@ -32,6 +35,7 @@ async function barghestToggleHybridForm(effect, disabled) {
 
   if (!disabled) {
     await actor.createEmbeddedDocuments('Item', [item]);
+    actor.items.find((i) => i.system.swid === 'barghest-hybrid-form-claws-fangs').system.favorite = favorite;
   } else {
     const itemToDelete = actor.items.find((i) => i.system.swid === itemSwid);
     if (itemToDelete) {
@@ -43,6 +47,7 @@ async function barghestToggleHybridForm(effect, disabled) {
 // Function to run when the Wolf Form toggle is updated
 async function barghestToggleWolfForm(effect, disabled) {
   const { actor } = effect;
+  const { favorite } = effect.flags.swade;
 
   const itemId = 'Oq37dJHD13zzefor';
   const itemSwid = 'barghest-wolf-form';
@@ -51,6 +56,8 @@ async function barghestToggleWolfForm(effect, disabled) {
 
   if (!disabled) {
     await actor.createEmbeddedDocuments('Item', [item]);
+    actor.items.find((i) => i.system.swid === 'go-for-the-throat').system.favorite = favorite;
+    actor.items.find((i) => i.system.swid === 'barghest-wolf-form-bite').system.favorite = favorite;
   } else {
     const itemToDelete = actor.items.find((i) => i.system.swid === itemSwid);
     if (itemToDelete) {
